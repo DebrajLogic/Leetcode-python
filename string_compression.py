33c1db823265d5cec13b6321d78155a32e802e83
DebrajLogic <debrajdey9696@gmail.com>
2024-03-22 07:59:07 -0400

    first try of string compression

---
+++ string_compression.py
@@ -0,0 +1,55 @@
+# Given an array of characters chars, compress it using the following algorithm:
+# Begin with an empty string s. For each group of consecutive repeating characters in chars:
+# If the group's length is 1, append the character to s.
+# Otherwise, append the character followed by the group's length.
+# The compressed string s should not be returned separately, but instead, be stored in the input character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars.
+# After you are done modifying the input array, return the new length of the array.
+# You must write an algorithm that uses only constant extra space.
+
+'''
+Example 1:
+Input: chars = ["a","a","b","b","c","c","c"]
+Output: Return 6, and the first 6 characters of the input array should be: ["a","2","b","2","c","3"]
+Explanation: The groups are "aa", "bb", and "ccc". This compresses to "a2b2c3".
+
+Example 2:
+Input: chars = ["a"]
+Output: Return 1, and the first character of the input array should be: ["a"]
+Explanation: The only group is "a", which remains uncompressed since it's a single character.
+
+Example 3:
+Input: chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+Output: Return 4, and the first 4 characters of the input array should be: ["a","b","1","2"].
+Explanation: The groups are "a" and "bbbbbbbbbbbb". This compresses to "ab12".
+'''
+
+
+from output_decorator import output
+
+
+@output
+def compress(chars):
+    char_count = {}
+
+    for char in chars:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+
+    chars = []
+    for key, value in char_count.items():
+        chars.append(str(key))
+
+        if value == 1:
+            continue
+
+        if value > 1:
+            for digit in str(value):
+                chars.append(digit)
+
+    return chars
+
+
+chars = ["a", "a", "a", "b", "b", "a", "a"]
+compress(chars)
--- trapping_rain_water.py
+++ trapping_rain_water.py
@@ -7,7 +7,10 @@ Output: 6
 Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
 '''
 
+from output_decorator import output
 
+
+@output
 def trap(height):
     left = [0]*len(height)
     right = [0]*len(height)
@@ -28,5 +31,4 @@ def trap(height):
 
 
 height = [3, 1, 2, 4, 0, 1, 3, 2]
-result = trap(height)
-print(result)
+trap(height)
